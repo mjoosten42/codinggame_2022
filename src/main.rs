0497608449f20bac46d3af09f132c7fda701eb69
mjoosten <mjoosten@student.codam.nl>
2022-12-17 20:54:41 +0100

    Fixed coordinated movement, spawn recyclers on front line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BTreeSet, HashSet}, fmt, cmp::min};
+use std::{collections::{BTreeSet, HashSet}, cmp::max, fmt, ops::{Add, AddAssign, Div, Sub, DivAssign}};
 use rand::{thread_rng, seq::SliceRandom};
 
 macro_rules! parse_input {
@@ -49,24 +49,70 @@ impl Actions {
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Point {
-	x: usize,
-	y: usize,
+struct IVec2 {
+	x: i32,
+	y: i32,
 }
 
-impl Point {
-	fn from(x: usize, y: usize) -> Self {
-		Point { x: x, y: y }
+impl IVec2 {
+	fn new(x: i32, y: i32) -> Self {
+		IVec2 { x: x, y: y }
+	}
+
+	fn perp(self: &Self) -> Self {
+		IVec2 { x: self.y, y: -self.x }
+	}
+
+	fn abs(self: &Self) -> i32 {
+		self.x.abs() + self.y.abs()
 	}
 }
 
-impl fmt::Display for Point {
+impl Add for IVec2 {
+	type Output = IVec2;
+
+	fn add(self, rhs: Self) -> Self::Output {	
+		IVec2 { x: self.x + rhs.x, y: self.y + rhs.y }
+	}
+}
+
+impl AddAssign for IVec2 {
+	fn add_assign(&mut self, rhs: Self) {
+		self.x += rhs.x;
+		self.y += rhs.y;
+	}
+}
+
+impl Sub for IVec2 {
+	type Output = IVec2;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		IVec2 { x: self.x - rhs.x, y: self.y - rhs.y }
+	}
+}
+
+impl Div<i32> for IVec2 {
+	type Output = IVec2;
+
+	fn div(self, rhs: i32) -> Self::Output {
+		IVec2 { x: self.x / rhs, y: self.y / rhs }
+	}
+}
+
+impl DivAssign<i32> for IVec2 {
+	fn div_assign(&mut self, rhs: i32) {
+		self.x /= rhs;
+		self.y /= rhs;
+	}
+}
+
+impl fmt::Display for IVec2 {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		write!(f, "[{}, {}]", self.x, self.y)
 	}
 }
 
-impl fmt::Debug for Point {
+impl fmt::Debug for IVec2 {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		write!(f, "[{}, {}]", self.x, self.y)
 	}
@@ -81,7 +127,8 @@ enum Owner {
 
 #[derive(Copy, Clone, Debug)]
 struct Patch {
-	position: Point,
+	position: IVec2,
+	scrap_total: usize,
 
 	scrap: usize,
 	owner: Owner,
@@ -93,9 +140,10 @@ struct Patch {
 }
 
 impl Patch {
-	fn from(x: usize, y: usize) -> Self {
+	fn from(x: i32, y: i32) -> Self {
 		Patch {
-			position: Point::from(x, y),
+			position: IVec2::new(x, y),
+			scrap_total: 0,
 			scrap: 0,
 			owner: Owner::Neutral,
 			units: 0,
@@ -107,32 +155,36 @@ impl Patch {
 	}
 }
 
-impl fmt::Display for Patch {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "[{}, {}]", self.position.x, self.position.y)
-	}
-}
-
 fn main() {
 	const COST: usize = 10;
 	let inputs = get_inputs();
 	let width = parse_input!(inputs[0], usize);
 	let height = parse_input!(inputs[1], usize);
 
-	let mut grid: Vec<Vec<Patch>> = (0..height).map(|j| (0..width).map(|i| Patch::from(i, j)).collect()).collect();
+	let mut grid: Vec<Vec<Patch>> = (0..height as i32).map(|j: i32| {
+		(0..width as i32).map(|i: i32| {
+			Patch::from(i, j)
+		}).collect()
+	}).collect();
 
 	loop {
 		let mut actions: Actions = Actions::new();
-		let mut my_matter = update_input(&mut grid, width, height);
+		let mut ally_matter = update_input(&mut grid, width, height);
 	
-		let mut units: Vec<Point> = Vec::new();
-		let mut ally_tiles: Vec<Patch> = Vec::new();
-		let mut ally_tiles_in_range_of_recycler = 0;
+		let mut ally_units: Vec<IVec2> = Vec::new();
+		let mut ally_tiles: Vec<IVec2> = Vec::new();
+		let mut ally_recyclers: Vec<IVec2> = Vec::new();
+		
+		let mut enemy_units: Vec<IVec2> = Vec::new();
 	
 		// Map data
 		for y in 0..height {
 			for x in 0..width {
+				let adj: Vec<Patch> = adjacent(&grid, IVec2::new(x as i32, y as i32)).into_iter().map(|p| get(&grid, p).clone()).collect();
+			
 				let patch = &mut grid[y][x];
+
+				patch.scrap_total = adj.iter().fold(patch.scrap, |sum, p| sum + p.scrap);
 
 				// Update scrap
 				if patch.in_range_of_recycler {
@@ -144,91 +196,104 @@ fn main() {
 						patch.units = 0;
 					}
 				}
-				// Save unit positions
-				if patch.owner == Owner::Ally {
-					ally_tiles.push(*patch);
-					if patch.units > 0 {
-						units.push(patch.position);
-				 	}
-					if patch.in_range_of_recycler {
-						ally_tiles_in_range_of_recycler += 1;
+
+				if patch.units > 0 {
+					if patch.owner == Owner::Ally {
+						ally_units.push(patch.position);
+					} else {
+						enemy_units.push(patch.position);
 					}
 				}
+
+				if patch.owner == Owner::Ally {
+					ally_tiles.push(patch.position);
+					if patch.recycler {
+						ally_recyclers.push(patch.position);
+				}
+			}
 
 			}
 		}
 
 		ally_tiles.sort_by(|lhs, rhs| {
-			let lhs_sum = adjacent(&grid, lhs.position).into_iter()
-				.fold(lhs.scrap, |sum, p| {
-					sum + grid[p.y][p.x].scrap
-				});
-			let rhs_sum = adjacent(&grid, rhs.position).into_iter()
-				.fold(rhs.scrap, |sum, p| {
-					sum + grid[p.y][p.x].scrap
-				});
-			lhs_sum.cmp(&rhs_sum)
+			get(&grid, *lhs).scrap_total.cmp(&get(&grid, *rhs).scrap_total)
 		});
 
-		ally_tiles = ally_tiles.into_iter().rev().collect();
+		let ally_gravity = ally_units.iter().fold(IVec2::new(0, 0), |sum, point| sum + *point) / max(ally_units.len() as i32, 1);
+		let enemy_gravity = enemy_units.iter().fold(IVec2::new(0, 0), |sum, point| sum + *point) / max(enemy_units.len() as i32, 1);
+		let center = (ally_gravity + enemy_gravity) / 2;
+
+		let perp = (center - ally_gravity).perp();
+
+		let frontline_start = center + perp;
+		let frontline_end = center - perp;
+
+		let frontline = line(frontline_start, frontline_end);
 
 		// Moving units
-		for unit in units {
-			for _ in 0..grid[unit.y][unit.x].units {
+		for unit in ally_units {
+			for _ in 0..get(&grid, unit).units {
 
-				let target = nearest_where(&grid, unit, |p| {
-					p.owner != Owner::Ally && !p.recycler
-				});
+				let target = nearest_where(&grid, unit, |p| p.owner != Owner::Ally);
 
 				if target.is_none() {
 					break ;
 				}
 
 				let point = target.unwrap();
-				let patch = &mut grid[unit.y][unit.x];
-				if patch.owner == Owner::Enemy {
-					if patch.units > 0 {
-						patch.units -= 1;
-					} else {
-						patch.owner = Owner::Ally;
-					}
+				let patch = get_mut(&mut grid, point);
+
+				if patch.units > 0 && (point - unit).abs() == 1 {
+					break ;
+				}
+			
+				if patch.units > 0 {
+					patch.units -= 1;
 				} else {
 					patch.owner = Owner::Ally;
 				}
-
 				MOVE!(actions, 1, unit, point);
 			}
 		}
 
 		// Building recyclers
-		let free_tiles = ally_tiles.len() - ally_tiles_in_range_of_recycler;
-		let build_budget = free_tiles - min(free_tiles, 20);
-		for _ in 0..(build_budget / COST) {
-			for patch in &ally_tiles {
-				let nearby = near(&grid, patch.position, 2);
-			
-				if my_matter >= COST && patch.can_build && !nearby.iter().any(|p| grid[p.y][p.x].recycler) {
-					let adj = adjacent(&grid, patch.position);
+		for tile in &ally_tiles {
+			let patch = get(&grid, *tile).clone();
+		
+			// Frontline
+			if ally_matter >= COST && patch.can_build && frontline.contains(tile) {
+				let patch = get_mut(&mut grid, *tile);
 				
-					if adj.iter().all(|p| grid[p.y][p.x].units == 0 || grid[p.y][p.x].owner != Owner::Ally) {
-						BUILD!(actions, patch.position);
-						grid[patch.position.y][patch.position.x].recycler = true;
-						my_matter -= COST;
-						break ;
-					}
-				}
+				BUILD!(actions, tile);
+				
+				patch.can_spawn = false;
+				patch.recycler = true;
+				ally_matter -= COST;
+				break ;
+			}
+
+			// Efficient patches
+			let nearby_recycler = near(&grid, *tile, 2).iter().any(|p| get(&grid, *p).recycler);
+			if ally_matter >= COST && patch.can_build && patch.scrap >= 5 && patch.scrap_total >= 40 && !nearby_recycler {
+				let patch = get_mut(&mut grid, *tile);
+			
+				BUILD!(actions, tile);
+			
+				patch.can_spawn = false;
+				patch.recycler = true;
+				ally_matter -= COST;
 			}
 		}
-	
-		let middle = nearest_where(&grid, Point::from(width / 2, height / 2), |p| {
-			p.can_spawn && p.scrap > 0
-		});
-	
-		if middle.is_some() {
-			let point = middle.unwrap();
 
-			if my_matter >= COST {
-				SPAWN!(actions, my_matter / COST, point);
+		// Spawning next to unowned tiles
+		for tile in &ally_tiles {
+			let patch = get(&grid, *tile);
+		
+			if patch.can_spawn &&  ally_matter >= COST && patch.units == 0 {
+				if adjacent_movable(&grid, *tile).iter().any(|p| get(&grid, *p).owner != Owner::Ally) {
+					SPAWN!(actions, 1, tile);
+					ally_matter -= COST;
+				} 
 			}
 		}
 
@@ -236,11 +301,37 @@ fn main() {
 	}
 }
 
-fn near(grid: &Vec<Vec<Patch>>, point: Point, dist: usize) -> Vec<Point> {
-	let mut points: HashSet<Point> = vec![point; 1].into_iter().collect();
+fn get<'a>(grid: &'a Vec<Vec<Patch>>, point: IVec2) -> &'a Patch {
+	&grid[point.y as usize][point.x as usize]
+}
+
+fn get_mut<'a>(grid: &'a mut Vec<Vec<Patch>>, point: IVec2) -> &'a mut Patch {
+	&mut grid[point.y as usize][point.x as usize]
+}
+
+fn line(mut start: IVec2, end: IVec2) -> Vec<IVec2> {
+	let mut points: Vec<IVec2> = vec![start];
+
+	while start != end {
+		let mut diff = end - start;
+
+		if diff.x.abs() < diff.y.abs() {
+			diff = IVec2::new(0, diff.y.clamp(-1, 1));
+		} else {
+			diff = IVec2::new(diff.x.clamp(-1, 1), 0);
+		}
+		start += diff;
+		points.push(start);
+	}
+
+	points
+}
+
+fn near(grid: &Vec<Vec<Patch>>, point: IVec2, dist: usize) -> Vec<IVec2> {
+	let mut points: HashSet<IVec2> = vec![point; 1].into_iter().collect();
 
 	for _ in 0..dist {
-		let mut new: Vec<Point> = Vec::new();
+		let mut new: Vec<IVec2> = Vec::new();
 		for p in &points {
 			new.extend(adjacent(grid, *p).into_iter());
 		}
@@ -250,16 +341,16 @@ fn near(grid: &Vec<Vec<Patch>>, point: Point, dist: usize) -> Vec<Point> {
 	points.into_iter().collect()
 }
 
-fn nearest_where<F>(grid: &Vec<Vec<Patch>>, point: Point, f: F) -> Option<Point> where
+fn nearest_where<F>(grid: &Vec<Vec<Patch>>, point: IVec2, f: F) -> Option<IVec2> where
 	F: Fn(&Patch) -> bool {
 	
 	let mut edges = vec![point; 1];
-	let mut visited: BTreeSet<Point> = vec![point; 1].into_iter().collect();
+	let mut visited: BTreeSet<IVec2> = vec![point; 1].into_iter().collect();
 	
 	while !edges.is_empty() {
-		let mut new: Vec<Point> = Vec::new();
+		let mut new: Vec<IVec2> = Vec::new();
 		for edge in &edges {
-			let patch = &grid[edge.y][edge.x];
+			let patch = get(grid, *edge);
 			if f(patch) {
 				return Some(edge.clone())
 			}
@@ -276,19 +367,19 @@ fn nearest_where<F>(grid: &Vec<Vec<Patch>>, point: Point, f: F) -> Option<Point>
 	None
 }
 
-fn adjacent_movable(grid: &Vec<Vec<Patch>>, point: Point) -> Vec<Point> {
-	adjacent(grid, point).into_iter().filter(|p| grid[p.y][p.x].scrap > 0).collect()
+fn adjacent_movable(grid: &Vec<Vec<Patch>>, point: IVec2) -> Vec<IVec2> {
+	adjacent(grid, point).into_iter().filter(|p| get(grid, *p).scrap > 0 && !get(grid, *p).recycler).collect()
 }
 
-fn adjacent(grid: &Vec<Vec<Patch>>, point: Point) -> Vec<Point> {
-	adjacent_tiles(point).into_iter().filter_map(|tile| {
+fn adjacent(grid: &Vec<Vec<Patch>>, point: IVec2) -> Vec<IVec2> {
+	adjacent_points(point).into_iter().filter_map(|tile| {
 		let row = grid.iter().nth(tile.1 as usize)?;
 		row.iter().nth(tile.0 as usize)?;
-		Some(Point { x: tile.0 as usize, y: tile.1 as usize })
+		Some(IVec2 { x: tile.0, y: tile.1 })
 	}).collect()
 }
 
-fn adjacent_tiles(point: Point) -> Vec<(i32, i32)> {
+fn adjacent_points(point: IVec2) -> Vec<(i32, i32)> {
 	let mut adj: Vec<(i32, i32)> = vec![
 		(point.x as i32 + 1, point.y as i32),
 		(point.x as i32, point.y as i32 + 1),
@@ -303,7 +394,7 @@ fn adjacent_tiles(point: Point) -> Vec<(i32, i32)> {
 fn update_input(grid: &mut Vec<Vec<Patch>>, width: usize, height: usize) -> usize {
 	let inputs = get_inputs();
 
-	let my_matter = parse_input!(inputs[0], i32);
+	let ally_matter = parse_input!(inputs[0], i32);
 	let _opp_matter = parse_input!(inputs[1], i32);
 
 	for y in 0..height {
@@ -323,7 +414,7 @@ fn update_input(grid: &mut Vec<Vec<Patch>>, width: usize, height: usize) -> usiz
 		}
 	}
 
-	my_matter as usize
+	ally_matter as usize
 }
 
 fn get_inputs() -> Vec<String> {
